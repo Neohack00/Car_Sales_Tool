08d809d7f7878e892b722cad1a0ae6bd766baf50
Neohack00 <neo@bylock.se>
2019-10-09 11:06:19 +0200

    Preparing project with methods, propperties and references. Setting up the rest of the Forms in

--- Car Sales Tool/Car_Sales.Businesss/Car.cs
+++ Car Sales Tool/Car_Sales.Businesss/Car.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Car_Sales.Businesss
 {
-    public class Class1
+    public class Car
     {
+        public string RegisterNumber { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public string Model { get; set; }
+        public string Yearmodel { get; set; }
+        public int Speed { get; set; }
+        public int TotalKmDrived { get; set; }
+        public int HorsePower { get; set; }
+        public int Price { get; set; }
+        public bool Secondhand { get; set; }
     }
 }
--- Car Sales Tool/Car_Sales.DAL/Repostory.cs
+++ Car Sales Tool/Car_Sales.DAL/Repostory.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Car_Sales.Businesss;
+	
 
 namespace Car_Sales.DAL
 {
-    public class Class1
+    public class Repostory
     {
+        List<Car> MyCars = new List<Car>();
+
+        public void Save(Car NewCar)
+        {
+            // - Sparar den nya bilen till databasen
+        }
+        public void GetList()
+        {
+            // -Hämtar allt som är lagrat och lagrar det i en list som returneras. 
+        }
+        public void Update(int id, string ChangeData)
+        {
+           // -Uppdaterar en specifik data i databasen med hjälp av Id och en specifik kolumn
+        }
     }
 }
