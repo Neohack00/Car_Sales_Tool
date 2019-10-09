d8d9d04b4091e74773486c65a6e996bdcaf38028
Neohack00 <neo@bylock.se>
2019-10-09 11:45:02 +0200

    Recreated Form1 in WindowsForm.Sales as MainForm, Planing for next commit Goal

--- Car Sales Tool/Windows.Car_Sales/Program.cs
+++ Car Sales Tool/Windows.Car_Sales/Program.cs
@@ -16,7 +16,7 @@ namespace Windows.Car_Sales
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
